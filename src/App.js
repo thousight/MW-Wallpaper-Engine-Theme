63092d3d13070123d8017af71163b7be06d4ba50
Mark Wen <markwenguojie94@gmail.com>
2018-11-09 21:08:09 -0500

    Added date time display

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { PureComponent } from 'react';
+import moment from 'moment';
+
+const timeFormat = 'h:mm:ss a'
+const dateFormat = 'ddd MM/DD/YYYY'
 
 class App extends PureComponent {
+  timeInterval = null
+
+  state = {
+    time: moment()
+  }
+
+  componentDidMount() {
+    this.timeInterval = setInterval(() => this.setState({ time : moment() }),1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timeInterval)
+  }
+
   render() {
+    const { time } = this.state
+
     return (
       <div className="wallpaper">
-        
+        <div className="datetime">
+          <h1>{time.format(dateFormat)}</h1>
+          <h1>{time.format(timeFormat)}</h1>
+        </div>
       </div>
     );
   }
