878ecf19d3d62cc9cc6e6dba8932715f6f1a9009
Mark Wen <markwenguojie94@gmail.com>
2018-11-11 23:13:15 -0500

    Reverted back to vanilla js

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # MW Wallpaper Engine Theme
-My personal Wallpaper Engine theme using React.js, heavily inspired by this [time lapse](https://steamcommunity.com/sharedfiles/filedetails/?id=824548506) theme.
+My personal Wallpaper Engine theme, heavily inspired by this [time lapse](https://steamcommunity.com/sharedfiles/filedetails/?id=824548506) theme.
 
+### About React
+I first tried using Create React App to create this theme, but Wallpaper Engine simply cannot import the built website, and I am not able to see any error messages to debug. At the end, vanilla JavaScript works.
---
+++ js/time.js
@@ -0,0 +1,65 @@
+function showTime(date) {
+  let str;
+  let am = "am";
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+  if (mm < 10) mm = "0" + mm;
+  let ss = date.getSeconds();
+  if (ss < 10) ss = "0" + ss;
+
+  if (hh > 12) {
+    hh = hh - 12;
+    am = "pm";
+  }
+  str = '<span class="thin">' + hh + ":" + mm + ":" + ss + " " + am + "</span>";
+
+  return str;
+}
+
+function showDate(date) {
+  let str;
+  let MM = date.getMonth() + 1;
+  let DD = date.getDate();
+  let YY = date.getFullYear();
+  let ww = date.getDay();
+
+  switch (ww) {
+    case 0:
+      ww = "Sun";
+      break;
+    case 1:
+      ww = "Mon";
+      break;
+    case 2:
+      ww = "Tue";
+      break;
+    case 3:
+      ww = "Wed";
+      break;
+    case 4:
+      ww = "Thu";
+      break;
+    case 5:
+      ww = "Fri";
+      break;
+    case 6:
+      ww = "Sat";
+      break;
+
+    default:
+      break;
+  }
+
+  str = '<span class="thin">' + ww + " " + MM + "/" + DD + "/" + YY + "</span>";
+
+  return str;
+}
+
+function tick() {
+  let today = new Date();
+  document.getElementById("date").innerHTML = showDate(today);
+  document.getElementById("time").innerHTML = showTime(today);
+  window.setTimeout("tick()", 1000);
+}
+
+tick();
